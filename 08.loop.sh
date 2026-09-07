47985f93ae276fe63561f85319b1cd647298884c
Raveendra Meesala <meesala10@gmail.com>
2026-09-07 22:46:19 +0530

    message

--- 08.loop.sh
+++ 08.loop.sh
@@ -3,41 +3,49 @@
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
 
 LOG_FOLDER="/var/log/shell-practice"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_NAME="$LOG_FOLDER/SCRIPT_NAME.log"
+
+SCRIPT_NAME=$(echo "$0" | cut -d "." -f1)
+
+LOG_NAME="$LOG_FOLDER/$SCRIPT_NAME.log"
+
 PACKAGES=("nginx" "mysql" "python")
 
-mkdir -p $LOG_FOLDER
+mkdir -p "$LOG_FOLDER"
 
 userid=$(id -u)
 
-if [ $userid -ne 0 ]
-then 
+if [ "$userid" -ne 0 ]
+then
     echo -e "$R Error :: please run with root user $N"
     exit 1
 else
     echo -e "$Y installing with root user $N"
 fi
 
-VALIDATE()
-if [ $1 -eq 0 ]
-then 
-    echo "$2 is installing SUCCESS"
-else
-    echo "$2 is installing failure"
-fi
+VALIDATE() {
+    if [ "$1" -eq 0 ]
+    then
+        echo "$2 is installing SUCCESS"
+    else
+        echo "$2 is installing FAILURE"
+    fi
+}
 
-for package in ${PACKAGES[@]}
+for package in "${PACKAGES[@]}"
 do
-  dnf list installed $package &>> $LOG_NAME
-if [ $? -ne 0 ]
-then
-        echo "please install install the $package server"
-        dnf install $package -y
+
+    dnf list installed "$package" &>> "$LOG_NAME"
+
+    if [ $? -ne 0 ]
+    then
+        echo "please install the $package server"
+        dnf install "$package" -y
         VALIDATE $? "$package"
-else
-    echo "already installed installed $package server no need" &>> $LOG_NAME
-fi
+    else
+        echo "already installed $package server, no need"
+    fi
+
 done
